781dcb10dd8efc729491b15134b1fa5523c15ef6
Monish Saravana <monish.sd.2008@Gmail.com>
2024-02-13 02:59:49 -0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,14 +1,14 @@
 from flask import Flask
 from util import elo
-# from util import main
+from util import main
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
 
-    # rating = main.getAvgElo(22012)
-    return str(elo.ratingDiff)
+    rating = main.getAvgElo(22012)
+    return str(22012) + " rating: " + str(rating)
 
 @app.route('/about')
 def about():
