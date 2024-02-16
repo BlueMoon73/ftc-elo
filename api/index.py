20ea74217fd6a75b431222c4661927f274a88185
Monish Saravana <monish.sd.2008@Gmail.com>
2024-02-15 19:11:13 -0500

    pls work

--- api/index.py
+++ api/index.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('page.html')
+    return render_template('api/page.html')
 
 @app.route('/elo/<teamnum>')
 def elo(teamnum):
