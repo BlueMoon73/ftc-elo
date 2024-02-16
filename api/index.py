ccc3b49a3055a2f195cd78f88e92fac028f5d801
Monish Saravana <monish.sd.2008@Gmail.com>
2024-02-15 20:34:28 -0500

    fixed matchup url parameters

--- api/index.py
+++ api/index.py
@@ -4,9 +4,11 @@ from util import main
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
     return render_template('page.html')
+
 
 @app.route('/elo', methods=['GET'])
 def elo():
@@ -15,17 +17,37 @@ def elo():
         return "teamnumber parameter is missing", 400
     rating = main.getAvgElo(int(teamnumber))
     return str(int(teamnumber)) + " rating: " + str(rating)
+
+
 @app.route('/elo/<teamnum>')
 def elo2(teamnum):
     rating = main.getAvgElo(int(teamnum))
     return str(int(teamnum)) + " rating: " + str(rating)
-@app.route('/elo?<teamnum>')
-def elo3(teamnum):
-    rating = main.getAvgElo(int(teamnum))
-    return str(int(teamnum)) + " rating: " + str(rating)
 
-@app.route('/matchup?team1=<team1>&team2=<team2>&team3=<team3>&team4=<team4>')
+
+@app.route('/matchup/<team1>&<team2>/<team3>&<team4>')
 def matchup(team1, team2, team3, team4):
+    predictions = main.predictMatches(int(team1), int(team2), int(team3), int(team4))
+
+    ret = str(team1) + " and " + str(team2) + " (" + str(round(predictions[-2] * 100, 2)) + "%)"
+    ret += " vs. "
+    ret += str(team3) + " and " + str(team4) + " (" + str(round(predictions[-1] * 100, 2)) + "%)"
+    return ret
+
+
+@app.route('/matchup?team1=<team1>&team2=<team2>&team3=<team3>&team4=<team4>', methods=['GET'])
+def matchup2(team1, team2, team3, team4):
+    team1 = request.args.get('team1')
+    team2 = request.args.get('team2')
+    team3 = request.args.get('team3')
+    team4 = request.args.get('team4')
+    if team1 or team3 is None:
+        return "teamnumber parameter is missing", 400
+    if team2 is None:
+        team2 = team1
+    if team4 is None:
+        team4 = team3
+
     predictions = main.predictMatches(int(team1), int(team2), int(team3), int(team4))
 
     ret = str(team1) + " and " + str(team2) + " (" + str(round(predictions[-2] * 100, 2)) + "%)"
