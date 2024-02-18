859cadb728b5398a26090ebc206d8475ec4d3c6f
Monish Saravana <monish.sd.2008@Gmail.com>
2024-02-17 22:05:25 -0500

    23eqwdcsxz

--- api/index.py
+++ api/index.py
@@ -12,16 +12,21 @@ def home():
 
 @app.route('/elo', methods=['GET'])
 def elo():
+
     teamnumber = request.args.get('teamnumber')
     if teamnumber is None:
         return "teamnumber parameter is missing", 400
-    rating = main.getAvgElo(int(teamnumber))
-    return render_template('elo.html', teamnumber=teamnumber, rating=round(rating,2))
+    if teamnumber.isnumeric():
+        rating = main.getAvgElo(int(teamnumber))
+        return render_template('elo.html', teamnumber=teamnumber, rating=round(rating,2))
+    else:
+        return render_template('error.html')
     # return str(int(teamnumber)) + " rating: " + str(rating)
 
 
 @app.route('/matchup', methods=['GET'])
 def matchup():
+
     team1 = request.args.get('team1')
     team2 = request.args.get('team2')
     team3 = request.args.get('team3')
@@ -30,6 +35,7 @@ def matchup():
         return "teamnumber parameter is missing", 400
     if team2 is None:
         team2 = team1
+        retyu
     if team4 is None:
         team4 = team3
 
